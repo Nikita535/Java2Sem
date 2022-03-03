d6ef55669f047ce5cacb6caa68f65f60285a5dcb
Nikita Zhiznevskiy <nzhigulevskiy@bk.ru>
2022-03-03 23:04:39 +0300

    16-18

--- pr1/src/ru/mirea/lab2/Main.java
+++ pr1/src/ru/mirea/lab2/Main.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) {
 
-       h
+
         Predicate<String> containsA = t -> t.matches("^([0-9]{4})$|^([0-9]{6})$");
 
         System.out.println(containsA.test("1234"));
